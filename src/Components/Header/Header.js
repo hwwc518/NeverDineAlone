d45ed488e60bbb8d294fb8e68372b142829c9752
k3vinfoo <kf1270@nyu.edu>
2017-09-08 21:51:20 -0400

    updated

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import headerImg from '../../static/img/header-img.jpg';
-import '../../static/css/App.css';
+import '../../static/css/Header.css';
 
 class App extends Component {
     render() {
         return (
             <div className = "App" >
                 <div className = "App-header">
-                    <h2> Never Dine Alone </h2>
-                    <a href="#" className="App-anchor" id="logIn">Log In</a>
+                    <div className = "header-Center">
+                        <h2> Never Dine Alone </h2>
+                        <a href="#" className="App-anchor" style={{float:'right'}}>Log In</a>
+                        <a href="#" className="App-anchor" style={{float:'right'}}>Sign Up</a>
+                    </div>
                 </div>
-                <p className = "App-intro"> To get started, edit <code> src / App.js </code> and save to reload. </p>
             </div>
         );
     }
