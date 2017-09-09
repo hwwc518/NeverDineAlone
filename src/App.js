f0f24952d3c0a567651e6edef1f04d7ce2518f36
Austin Vuong <vuongaustin@gmail.com>
2017-09-09 11:35:19 -0400

    Autosuggest and College Info

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import CollegeProfile from './Components/Body/Profile/CollegeProfile'
 import CollegeStudentProfile from './Components/Body/Profile/CollegeStudentProfile'
 
 //import Footer from './Components/Footer'
+import CollegeInfo from './Components/CollegeInfo'
+
 
 import './static/css/App.css';
 
