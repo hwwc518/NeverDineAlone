85fd711b4ac78d45cd99dc9aa9e12d98ed312d5b
Huiwah <hwwc518@gmail.com>
2017-09-09 18:52:45 -0400

    added sign in page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import Header from './Components/Header/Header'
-import CollegeProfile from './Components/Body/Profile/CollegeProfile'
-import CollegeStudentProfile from './Components/Body/Profile/CollegeStudentProfile'
-
+import Header from './Components/Header/Header';
+import CollegeProfile from './Components/Body/Profile/CollegeProfile';
+import CollegeStudentProfile from './Components/Body/Profile/CollegeStudentProfile';
+import SignUp from './Components/SignUp';
 //import Footer from './Components/Footer'
-import CollegeInfo from './Components/CollegeInfo'
+import CollegeInfo from './Components/CollegeInfo';
 
 
 import './static/css/App.css';
+import './static/css/SignUp.css';
 
 class App extends Component {
     render() {
         return (
-			<Header/>
+			<SignUp/>
 			//<CollegeStudentProfile/>
         );
     }
