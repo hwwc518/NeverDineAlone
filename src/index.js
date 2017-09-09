79c6135e3695511a34663dcf1a0d12dc26ec628d
Huiwah <hwwc518@gmail.com>
2017-09-09 01:38:43 -0400

    deploying firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './static/css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyD1j-jKv77ffwom2sz4A8Jb7NWLrc8rPs8",
+    authDomain: "sliceofcollege-1fcaa.firebaseapp.com",
+    databaseURL: "https://sliceofcollege-1fcaa.firebaseio.com",
+    projectId: "sliceofcollege-1fcaa",
+    storageBucket: "",
+    messagingSenderId: "79099187210"
+  };
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
