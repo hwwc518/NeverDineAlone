5063f347309691fee5f2248ed1169041841f23cc
k3vinfoo <kf1270@nyu.edu>
2017-09-09 00:00:19 -0400

    updated with homepage

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import headerImg from '../../static/img/header-img.jpg';
 import '../../static/css/Header.css';
 
+
 class App extends Component {
     render() {
         return (
-            <div className = "App" >
+            <header>
+            <div className = "App">
                 <div className = "App-header">
                     <div className = "header-Center">
-                        <h2> Never Dine Alone </h2>
+                        <h2> Dorm Mate </h2>
                         <a href="#" className="App-anchor" style={{float:'right'}}>Log In</a>
                         <a href="#" className="App-anchor" style={{float:'right'}}>Sign Up</a>
                     </div>
+                    <div className = "search-cont">
+                        <form>
+                            <label>
+                                <input className= "inputBox" type="text" name="search" placeholder="Search Universities/Colleges" required/>
+
+                            </label>
+                            <button className= "btn-search" type="submit" value="Submit"> Go </button>
+                        </form>
+                    </div>
                 </div>
             </div>
+            </header>
         );
     }
 }
