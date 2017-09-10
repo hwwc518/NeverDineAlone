1ab050257ab8c70c4d9437a4a8c1a47c3d08b380
Simon Chen <sc4900@nyu.edu>
2017-09-10 03:22:23 -0400

    working search

--- src/Components/CollegeInfo.js
+++ src/Components/CollegeInfo.js
@@ -14,35 +14,40 @@ class CollegeInfo extends Component {
 		this.state = {collegePctAdmitted: ""};
   	}
 	
-	componentWillMount() {		
-		for (var i = 0; i < data.length; i++) {			
-			var obj = data[i];
-			if (obj.FIELD1 == "University of Pennsylvania") {
-				this.state.collegeName = obj.FIELD1;
-				
-				if (obj.FIELD2 == "11") { this.state.collegeHiDegree = "Doctor's degree - research/scholarship and professional practice"; }
-				else if (obj.FIELD2 == "12") { this.state.collegeHiDegree = "Doctor's degree - research/scholarship"; }
-				else if (obj.FIELD2 == "13") { this.state.collegeHiDegree = "Doctor's degree -  professional practice"; }
-				else if (obj.FIELD2 == "14") { this.state.collegeHiDegree = "Doctor's degree - other"; }
-				else if (obj.FIELD2 == "20") { this.state.collegeHiDegree = "Master's degree"; }
-				else if (obj.FIELD2 == "30") { this.state.collegeHiDegree = "Bachelor's degree"; }
-				else if (obj.FIELD2 == "40") { this.state.collegeHiDegree = "Associate's degree"; }
-				else if (obj.FIELD2 == "0") { this.state.collegeHiDegree = "Non-degree granting"; }
-				else if (obj.FIELD2 == "-3") { this.state.collegeHiDegree = "Unavailable"; }
-				
-				if (obj.FIELD3 == "1") { this.state.collegeType = "Four or more years"; }
-				else if (obj.FIELD3 == "2") { this.state.collegeType = "At least 2 but less than 4 years"; }
-				else if (obj.FIELD3 == "3") { this.state.collegeType = "Less than 2 years (below associate)"; }
-				else if (obj.FIELD3 == "-3") { this.state.collegeType = "Unavailable"; }
-				
-				this.state.collegeInStatePrice = obj.FIELD4;
-				this.state.collegeOutStatePrice = obj.FIELD5;
-				this.state.collegeAdmissionsYield = obj.FIELD6;
-				this.state.collegePctAdmitted = obj.FIELD7;				
-			}
-		}
-	}
-	
+    componentWillMount() {
+        var ind = window.location.href.indexOf('college');
+        var college = window.location.href.substring(ind+8).split("%20").join(" ");
+        
+        console.log(college);
+        
+        for (var i = 0; i < data.length; i++) {			
+	    var obj = data[i];
+	    if (obj.FIELD1 == college) {
+	        this.state.collegeName = obj.FIELD1;
+	        
+	        if (obj.FIELD2 == "11") { this.state.collegeHiDegree = "Doctor's degree - research/scholarship and professional practice"; }
+	        else if (obj.FIELD2 == "12") { this.state.collegeHiDegree = "Doctor's degree - research/scholarship"; }
+	        else if (obj.FIELD2 == "13") { this.state.collegeHiDegree = "Doctor's degree -  professional practice"; }
+	        else if (obj.FIELD2 == "14") { this.state.collegeHiDegree = "Doctor's degree - other"; }
+	        else if (obj.FIELD2 == "20") { this.state.collegeHiDegree = "Master's degree"; }
+	        else if (obj.FIELD2 == "30") { this.state.collegeHiDegree = "Bachelor's degree"; }
+	        else if (obj.FIELD2 == "40") { this.state.collegeHiDegree = "Associate's degree"; }
+	        else if (obj.FIELD2 == "0") { this.state.collegeHiDegree = "Non-degree granting"; }
+	        else if (obj.FIELD2 == "-3") { this.state.collegeHiDegree = "Unavailable"; }
+	        
+	        if (obj.FIELD3 == "1") { this.state.collegeType = "Four or more years"; }
+	        else if (obj.FIELD3 == "2") { this.state.collegeType = "At least 2 but less than 4 years"; }
+	        else if (obj.FIELD3 == "3") { this.state.collegeType = "Less than 2 years (below associate)"; }
+	        else if (obj.FIELD3 == "-3") { this.state.collegeType = "Unavailable"; }
+	        
+	        this.state.collegeInStatePrice = obj.FIELD4;
+	        this.state.collegeOutStatePrice = obj.FIELD5;
+	        this.state.collegeAdmissionsYield = obj.FIELD6;
+	        this.state.collegePctAdmitted = obj.FIELD7;				
+	    }
+        }
+    }
+    
     render(collegeName) {
         return (
 			<div>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,50 +6,63 @@ import 'react-power-select/dist/react-power-select.css'
 import { PowerSelect } from 'react-power-select'
 
 class App extends Component {
-	constructor(props) {
+    constructor(props) {
     	super(props);
-		this.state = {colleges: []};
+	this.state = {colleges: []};
+         }
+    
+    componentWillMount () {
+	for (var i = 0; i < collegesArray.length; i++) {
+	    this.state.colleges.push(collegesArray[i].FIELD1);
 	}
-	componentWillMount () {
-		for (var i = 0; i < collegesArray.length; i++) {
-			this.state.colleges.push(collegesArray[i].FIELD1);
-		}
-	}
-	
-	state = {
-		selectedCollege: null,
-	};
+    }
 
-	handleChange = ({ option }) => {
-		this.setState({ selectedCollege: option });
-	};
-	
+    getCollegeInfo() {
+        console.log("I am called");
+        var college = document.getElementsByClassName("PowerSelect__TriggerLabel")[0];
+        var onLoad = true;
+        if (onLoad && (college.innerText == null || college.innerText == "") ){
+            onLoad = !onLoad;
+        }else
+            window.location.replace("http://localhost:3000/search?college="+college.innerText);
+
+    }
+    
+    state = {
+	selectedCollege: null,
+    };
+
+    handleChange = ({ option }) => {
+        this.setState({ selectedCollege: option });
+    };
+    
     render() {
         return (
             <header>
             <div className = "App">
-                <div className = "App-header">
-                    <div className = "header-Center">
-                        <h2> Dorm Mate </h2>
-                        <a href="#" className="App-anchor" style={{float:'right'}}>Log In</a>
-                        <a href="#" className="App-anchor" style={{float:'right'}}>Sign Up</a>
-                    </div>
-                    <div className = "search-cont">
-                        <form>
-                            <div className="demo">
-								<h1>Search Colleges</h1>
-								<PowerSelect
-								className="searcher"
-								options={this.state.colleges}
-								selected={this.state.selectedCollege}
-								optionLabelPath="name"
-								searchIndices={['name', 'code']}
-								onChange={this.handleChange}
-								/>
-							</div>
-                            <button className= "btn-search" type="submit" value="Submit"> Go </button>
-                        </form>
-                    </div>
+              <div className = "App-header">
+                <div className = "header-Center">
+                  <h2> Dorm Mate </h2>
+                  <a href="#" className="App-anchor" style={{float:'right'}}>Log In</a>
+                  <a href="#" className="App-anchor" style={{float:'right'}}>Sign Up</a>
+                </div>
+                <div className = "search-cont">
+
+                  <div className="demo">
+		    <h1>Search Colleges</h1>
+		    <PowerSelect
+	               className="searcher"
+	               options={this.state.colleges}
+	               selected={this.state.selectedCollege}
+	               optionLabelPath="name"
+	               searchIndices={['name', 'code']}
+	               onChange={this.handleChange}
+		       />
+		  </div>
+                  <button className= "btn-search" type="submit" value="Submit"
+                          onClick={this.getCollegeInfo}> Go </button>
+
+                </div>
                 </div>
             </div>
             </header>
